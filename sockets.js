cf162df34227d42e2cfb5224683a9ba58a07c4c6
Awari Moshood Dell <moshood@tractive.com.my>
2017-09-09 17:46:20 +0800

    remove routers from macs

--- sockets.js
+++ sockets.js
@@ -1,6 +1,6 @@
 var net = require('net');
 var mongojs = require('mongojs');
-var db = mongojs('mongodb://moshood:mosh1234@ds053972.mlab.com:53972/suretouch', ['Entries', 'Macs']);
+var db = mongojs('mongodb://moshood:mosh1234@ds053972.mlab.com:53972/suretouch', ['Entries', 'Macs', 'Routers']);
 var entry = {};
 
 var HOST = '61.6.23.70';
@@ -54,44 +54,50 @@ var passMacs = function (data) {
             timestamp: element.timestamp,
             createdAt: element.timestamp
         }
-        db.Macs.findOne({
+        db.Routers.findOne({
             sensorID: mac.sensorID,
             mac: mac.mac,
-            timestamp: {
-                $gte: today
-            }
         }, function (err, doc) {
-            if (doc != null) {
-                db.Macs.update(
-                    {
-                        sensorID: mac.sensorID,
-                        mac: mac.mac,
-                        timestamp: {
-                            $gte: today
-                        }
-                    },
-                    {
-                        $set: {
-                            rssi: mac.rssi,
-                            timestamp: mac.timestamp
-                        }
-                    },
-                    {
-                        upsert: true,
-                        multi: true
+            if (doc == null) {
+                db.Macs.findOne({
+                    sensorID: mac.sensorID,
+                    mac: mac.mac,
+                    timestamp: {
+                        $gte: today
                     }
-                )
-                console.log("Updated Entry " + mac.sensorID + " mac: " + mac.mac);
-            }
-            else {
-                db.Macs.save(mac, function (err, res) {
-                    if (err)
-                        console.log('err', err);
-                    console.log("Created Entry " + mac.sensorID + " mac: " + mac.mac);
-                });
+                }, function (err, doc) {
+                    if (doc != null) {
+                        db.Macs.update(
+                            {
+                                sensorID: mac.sensorID,
+                                mac: mac.mac,
+                                timestamp: {
+                                    $gte: today
+                                }
+                            },
+                            {
+                                $set: {
+                                    rssi: mac.rssi,
+                                    timestamp: mac.timestamp
+                                }
+                            },
+                            {
+                                upsert: true,
+                                multi: true
+                            }
+                        )
+                        // console.log("Updated Entry " + mac.sensorID + " mac: " + mac.mac);
+                    }
+                    else {
+                        db.Macs.save(mac, function (err, res) {
+                            if (err)
+                                console.log('err', err);
+                            // console.log("Created Entry " + mac.sensorID + " mac: " + mac.mac);
+                        });
+                    }
+                })
             }
         })
-
     }, this);
 }
 
@@ -99,7 +105,6 @@ function timeConverter(timestamp) {
     const time = new Date(timestamp * 1000);
     return time.toLocaleString();
 }
-
 
 function sockets() {
     clientConnect();
@@ -114,9 +119,51 @@ function sockets() {
             if (err)
                 console.log('err', err);
 
-            passMacs(entry);
+            var pass = new Promise((resolve, reject) => {
+                var sensorID = entry.sensorid;
+                if (entry.ssid != undefined && entry.ssid.length > 0) {
+                    entry.ssid.forEach(function (element, index) {
+                        var mac = {
+                            ssid: element.ssid,
+                            mac: element.mac,
+                            rssi: element.rssi,
+                            timestamp: element.timestamp,
+                            createdAt: element.timestamp
+                        }
+                        db.Routers.update(
+                            {
+                                sensorID: mac.sensorID,
+                                mac: mac.mac,
+                                ssid: mac.ssid
+                            },
+                            {
+                                $set: {
+                                    rssi: mac.rssi,
+                                    timestamp: mac.timestamp
+                                }
+                            },
+                            {
+                                upsert: true,
+                                multi: true
+                            }
+                        )
+                        if (index >= entry.ssid.length - 1) {
+                            resolve(entry);
+                        }
+
+                    }, this);
+                }
+                else {
+                    resolve(entry);
+                }
+            })
+
+            pass.then((data) => {
+                passMacs(data)
+            })
         });
     });
+
 
     client1.on('data', function (data) {
         today = new Date().setHours(0, 0, 0, 0) / 1000;
@@ -126,7 +173,47 @@ function sockets() {
             if (err)
                 console.log('err', err);
 
-            passMacs(entry);
+            var pass = new Promise((resolve, reject) => {
+                var sensorID = data.sensorid;
+                if (entry.ssid != undefined && entry.ssid.length > 0) {
+                    entry.ssid.forEach(function (element, index) {
+                        var mac = {
+                            sensorID: sensorID,
+                            mac: element.mac,
+                            rssi: element.rssi,
+                            timestamp: element.timestamp,
+                            createdAt: element.timestamp
+                        }
+                        db.Routers.update(
+                            {
+                                sensorID: mac.sensorID,
+                                mac: mac.mac
+                            },
+                            {
+                                $set: {
+                                    rssi: mac.rssi,
+                                    timestamp: mac.timestamp
+                                }
+                            },
+                            {
+                                upsert: true,
+                                multi: true
+                            }
+                        )
+                        if (index >= entry.ssid.length - 1) {
+                            resolve(entry);
+                        }
+
+                    }, this);
+                }
+                else {
+                    resolve(entry);
+                }
+            })
+
+            pass.then((data) => {
+                passMacs(data)
+            })
         });
     });
 

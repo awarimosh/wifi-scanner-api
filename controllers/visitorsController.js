1c8ffcc32fcaa7e70e60b88cf00dcb6d85d9be10
Awari Moshood Dell <moshood@tractive.com.my>
2017-10-05 00:20:24 +0800

    readUnique added

--- controllers/visitorsController.js
+++ controllers/visitorsController.js
@@ -3,14 +3,11 @@ var util = require('../helpers/controllerUtil');
 var db = require('../helpers/dbUtil');
 
 exports.read = function (req, res, next) {
-
     function resResult(result) {
-        // console.log(result);
         res.status(200).json(util.returnResultObject(result));
     }
 
     function resError(err) {
-        console.log(err);
         res.status(500).json(util.returnErrorObject(err));
     }
 
@@ -59,9 +56,63 @@ exports.read = function (req, res, next) {
         }, resError)
         .then(resResult, resError)
         .catch(resResult);
-
-
 };
+
+exports.readUnique = function(req, res, next){
+    function resResult(result) {
+        res.status(200).json(util.returnResultObject(result));
+    }
+
+    function resError(err) {
+        res.status(500).json(util.returnErrorObject(err));
+    }
+
+    var week = parseInt(req.query.week);
+    var year = parseInt(req.query.year);
+    var sensors = req.query.sensors.split(',');
+    var thisWeekdates = getTimestampFromWeek(week, year);
+    var lastWeekdates = getTimestampFromWeek(week - 1, year);
+    var nextWeekdates = getTimestampFromWeek(week + 1, year);
+    var resData = {};
+
+    getUniqueMacs(thisWeekdates, sensors)
+        .then(
+        function (data) {
+            return new Promise(function (resolve, reject) {
+                var result = formatData(data, thisWeekdates);
+                if (result === undefined)
+                    reject()
+                resolve(result);
+            });
+        }, resError)
+        .then(function (data) {
+            resData.thisweek = data.res;
+            return getUniqueMacs(lastWeekdates, sensors)
+        }, resError)
+        .then(function (data) {
+            return new Promise(function (resolve, reject) {
+                var result = formatData(data, lastWeekdates);
+                if (result === undefined)
+                    reject()
+                resolve(result);
+            });
+        }, resError)
+        .then(function (data) {
+            resData.lastweek = data.res;
+            return getUniqueMacs(nextWeekdates, sensors)
+        }, resError)
+        .then(function (nxt) {
+            return new Promise(function (resolve, reject) {
+                var result = formatData(nxt, nextWeekdates);
+                if (result === undefined)
+                    reject()
+                resData.nextweek = result.res;
+                resolve(resData);
+            });
+        }, resError)
+        .then(resResult, resError)
+        .catch(resResult);
+}
 
 var getTimestampFromWeek = function (weekNo, year) {
     weekNo = weekNo - 1;
@@ -104,7 +155,34 @@ var getMacs = function (dates, sensors) {
                 }
             })
         }, this);
+    })
+}
 
+var getUniqueMacs = function (dates, sensors) {
+    var res = [];
+    var jsonVar = {};
+    return new Promise(function (resolve, reject) {
+        sensors.forEach(function (element, index, array) {
+            db.macs().Macs.find({
+                timestamp: {
+                    $gte: dates.start,
+                    $lt: dates.end
+                },
+                sensorID: parseInt(element),
+                unique:true
+            }).skip(0, function (err, result) {
+                if (err) {
+                    reject(err);
+                }
+                jsonVar = {};
+                jsonVar.id = element;
+                jsonVar.data = result;
+                res.push(jsonVar);
+                if (res.length === array.length) {
+                    resolve(res);
+                }
+            })
+        }, this);
     })
 }
 
--- routes/visitors.js
+++ routes/visitors.js
@@ -3,5 +3,6 @@ var router = express.Router();
 var visitorsController = require('../controllers/visitorsController');
 
 router.get('/', visitorsController.read);
+router.get('/unique', visitorsController.readUnique);
 
 module.exports = router;
